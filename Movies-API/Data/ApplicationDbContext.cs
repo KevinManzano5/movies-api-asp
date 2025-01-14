a6f52eea90ec49094ec219a802d30138f5dd619e
Kevin Alejandro Manzano Torres <manzanokevin5@gmail.com>
2025-01-13 20:07:12 -0500

    connect to postgres db and create first migration

---
+++ Movies-API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies_API.Models;
+
+namespace Movies_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
